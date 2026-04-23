11d955b4b39ad2cad355f9128e335801ac40591d
sarahhlim <s10268585@connect.np.edu.sg>
2026-04-23 14:46:06 +0800

    Initial commit

---
+++ Assets/myfirstspecialscript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class myfirstspecialscript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("hello world!!!!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
